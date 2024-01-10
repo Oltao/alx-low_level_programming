eaf3a99fa30a6b163304cbb8e871afc63fad5fb2
Olajire Taofeek <taofeeqolajire@gmail.com>
2024-01-10 11:48:30 -0800

    fixed

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,22 +1,14 @@
-include "main.h"
+#include "main.h"
 
 /**
  * _strlen_recursion - Length of String
  * @s: string
  */
-
 int _strlen_recursion(char *s)
-
 {
-
 	if (*s != '\0')
-
 	{
-
 		return (1 + _strlen_recursion(s + 1));
-
 	}
-
 	return (0);
-
 }
