7d0b687b55562510a26013f4fd1c604f477ec2b3
Olajire Taofeek <taofeeqolajire@gmail.com>
2024-01-10 12:11:16 -0800

    fixed betty error

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,7 +16,9 @@ int sqrt2(int a, int b)
 		return (-1);
 	return (sqrt2(a, b + 1));
 }
-
+/**
+ * _sqrt_recursion: return natural square root
+ **/
 int _sqrt_recursion(int n)
 {
 	return (sqrt2(n, 1));
