c528b686bd0c2e2c7f50e106295fea6fdc85d131
Olajire Taofeek <taofeeqolajire@gmail.com>
2024-01-12 08:25:08 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - To print number of arguements passed.
+ * Return: 0.
+ * @argc: return counts parameter.
+ */
+
+int main(int argc)
+{
+	printf("%d\n", argc);
+	return (0);
+}
