fc4d9ca95527ba074e7efd004fd27bbd8ab7b396
Olajire Taofeek <taofeeqolajire@gmail.com>
2024-01-12 09:20:04 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,10 +5,11 @@
  * main - To print number of arguements passed.
  * Return: 0.
  * @argc: return counts parameter.
+ * @argv: Arguement vector but not used.
  */
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("Result is = %d\n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
