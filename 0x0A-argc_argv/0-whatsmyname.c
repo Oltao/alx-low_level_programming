69456b006b18158c7806e06111060c04221d89ec
Olajire Taofeek <taofeeqolajire@gmail.com>
2024-01-12 08:37:21 -0800

    fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 /**
 * main - program that print its name.
 * Return: 0.
-* @argc: Arguement count
+* @argc: Arguement count in number.
 * @argv: Arguement Value.
 */
 
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - To print number of arguements passed.
  * Return: 0.
  * @argc: return counts parameter.
  */
 
-int main(int argc, char *argv[])
+int main(int argc)
 {
-	printf("%d\n", argc);
+	printf("Result is = %d\n", argc);
 	return (0);
 }
