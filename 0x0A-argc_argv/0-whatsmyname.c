0bae3ca3b5b9ccfe3ef7e309734a675f972fa1bd
Olajire Taofeek <taofeeqolajire@gmail.com>
2024-01-12 07:24:17 -0800

    Fixed Errors

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 /**
- * main - program that print its name.
- * Return: 0.
- */
+* main - program that print its name.
+* Return: 0.
+* @argc: Arguement count
+* @argv: Arguement Value.
+*/
 
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
-	return(0);
+	return (0);
 }
