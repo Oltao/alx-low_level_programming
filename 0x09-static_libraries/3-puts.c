aab2442e0890a3e668cd1db39f4f9acc8f103955
Olajire Taofeek <taofeeqolajire@gmail.com>
2024-01-12 16:45:30 -0800

    fixed

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,6 +1,16 @@
-#include "3-puts.c"
+#include "main.h"
 
-void _puts(char *s)
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ */
+void _puts(char *str)
 {
-	return(0);
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
