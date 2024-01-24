1d312083689e3551f2c87842f5cb9240bcf7ab38
Olajire Taofeek <taofeeqolajire@gmail.com>
2024-01-24 10:18:46 -0800

    finally

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+
+/**
+ * op_add - func to calculates the sum of two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - func  calculates the difference of two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - func to calculates the product of two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - func to calculates the division of two integers
+ * @a: first int
+ * @b: second int
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - func to calculates the remainder of the division of two integers
+ * @a: first int
+ * @b: second int
+ * Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
